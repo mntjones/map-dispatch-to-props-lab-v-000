147c198d79f80c5f236cc0e286450305d406f000
Monica Tamaru Jones <mntamaru@gmail.com>
2018-11-19 18:37:50 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import RestaurantInput from './components/RestaurantInput';
 import Restaurants from './components/Restaurants';
 
+
 export class App extends Component {
   render() {
     return (
--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -17,13 +17,13 @@ export class RestaurantInput extends Component {
 
   handleOnLocationChange = event => {
     this.setState({
-      [event.target.id]: event.target.value
+      location: event.target.value
     });
   }
 
   handleOnSubmit = event => {
     event.preventDefault();
-    // add missing code
+    this.props.addRestaurant(this.state);
   }
 
   render() {
@@ -33,6 +33,7 @@ export class RestaurantInput extends Component {
           <input
             type="text"
             onChange={(event) => this.handleOnNameChange(event)}
+            value={this.state.name}
             id="name"
             placeholder="restaurant name" />
         </p>
@@ -40,6 +41,7 @@ export class RestaurantInput extends Component {
           <input
             type="text"
             onChange={(event) => this.handleOnLocationChange(event)}
+            value={this.state.location}
             id="location"
             placeholder="location" />
         </p>
@@ -51,4 +53,4 @@ export class RestaurantInput extends Component {
 
 
 //connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+export default connect( null, {addRestaurant})(RestaurantInput);
