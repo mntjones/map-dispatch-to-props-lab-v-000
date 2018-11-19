1ffb2e015f08481dd6f8eba86c400e96059e1618
Monica Tamaru Jones <mntamaru@gmail.com>
2018-11-19 18:31:57 +0000

    automatically backed up by learn

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -53,4 +53,4 @@ export class RestaurantInput extends Component {
 
 
 //connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+export default connect() RestaurantInput
